a48bcfcba46bae48bd9eefe3460a1b7b760b3d39
Gabe Jensen <thegabejensen@gmail.com>
2017-05-06 17:19:56 -0700

    added home and about pages

--- shallowsandbox/views.py
+++ shallowsandbox/views.py
@@ -2,10 +2,32 @@
 Define the app object, and all the routes
 """
 
+from flask import render_template
 from shallowsandbox import app
 
 @app.route('/')
 @app.route('/index')
-def home():
-    """ The home page """
-    return "Hello World!"
+def index():
+    """ The index page """
+    user = {'nickname': 'Gabe'}
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'I like lighting things on fire.'
+        },
+        {
+            'author': {'nickname': 'Bob'},
+            'body': 'Jim Gaffigan is my spirit animal.'
+        },
+        {
+            'author': {'nickname': 'Freddy'},
+            'body': 'I can eat an entire cow.'
+        }
+    ]
+
+    return render_template('index.html', user=user, posts=posts)
+
+@app.route('/about')
+def about():
+    """ The about page """
+    return render_template('about.html')
