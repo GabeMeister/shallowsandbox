8eb674a5a8f9d8f18333b296422b3963ac52780f
Gabe Jensen <thegabejensen@gmail.com>
2017-05-30 21:03:53 -0700

    Added nullable=False to all user model properties

--- shallowsandbox_app/models/user.py
+++ shallowsandbox_app/models/user.py
@@ -7,6 +7,6 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(50))
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
